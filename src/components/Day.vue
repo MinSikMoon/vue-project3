ded02403ad56a1baa2f1ac3438594da6df5cb69c
MinSikMoon <msmoon.business@gmail.com>
2018-09-04 23:28:55 +0900

    day, dayscore

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -3,16 +3,79 @@
     <h1 class="day-title">
       {{$route.params.date}}
     </h1>
+
+  <ul class="day-list">
+    <li
+      v-for="item in items"
+      :key="item.id"
+      class="day-list-item">
+      <div class="time">
+        <h2>
+          {{item.id}}
+        </h2>
+      </div>
+
+      <div class="action">
+        <input type="text" v-model="item.action"
+        placeholder="한 일을 작성해주세요.">
+      </div>
+      <day-score
+      @onUpdateScore="onUpdateScore"
+      :item="item">
+      </day-score>
+      <div class="note">
+        <textarea v-model="item.note"
+        placeholder="노트를 작성해 주세요.">
+        </textarea>
+      </div>
+    </li>
+  </ul>
   </div>
 </template>
 
 <script>
+import DayScore from './DayScore'
 export default {
   name: 'Day',
   props: {
-    msg: String
-  }
-}
+   
+  },
+  components: {
+    DayScore
+  },
+  methods: {
+    onUpdateScore(item, score){
+      item.score = score;
+    }
+  },
+  data() {
+    return {
+      items: [
+        {
+          action: '',
+          id: '2018-01-01-08-10',
+          note: '',
+          open: false,
+          score: undefined,
+        },
+        {
+          action: '',
+          id: '2018-01-01-09-11',
+          note: '',
+          open: false,
+          score: undefined,
+        },
+        {
+          action: '',
+          id: '2018-01-01-12-11',
+          note: '',
+          open: false,
+          score: undefined,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/DayScore.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="score">
+	  <button
+	  v-for="btn in buttons"
+	  :key="btn.score"
+	  :class="{'active':btn.score === item.score}"
+	  @click="updateScore(btn.score)"
+	  >
+		{{btn.label}}
+	  </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DayScore',
+  methods: {
+    updateScore(score) {
+		console.log(score);
+      this.$emit('onUpdateScore', this.item, score);
+    },
+  },
+  computed: {},
+  data() {
+    return {
+      buttons: [
+        {label: 'GOOD', score: 3},
+        {label: 'SOSO', score: 2},
+        {label: 'BAD', score: 1},
+      ],
+    };
+  },
+  props: ['item'],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
